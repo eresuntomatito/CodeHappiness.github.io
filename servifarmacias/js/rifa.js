b6e58651a168fef730474332fc884ef8b7741a72
CodeHappiness <sick4tacos@gmail.com>
2015-07-28 10:58:44 -0500

    modified:   servifarmacias/css/micss.css 	modified:   servifarmacias/files/registroRifa.txt 	modified:   servifarmacias/index.html 	modified:   servifarmacias/js/rifa.js 	servifarmacias/administrador.php 	servifarmacias/files/nuevaImagen.jpg 	servifarmacias/index.php 	servifarmacias/js/loginAdmin.js 	servifarmacias/js/showFormsAdmin.js 	servifarmacias/php/

--- servifarmacias/js/rifa.js
+++ servifarmacias/js/rifa.js
@@ -1,27 +1,41 @@
 function validateEntry(formID){
 //aqui se va a leer el archivo de registro para ver si ya esta registrado el ticket
-	
+	return true;	
 
 }
 
 function registerEntry(formID){
+	var form1 = document.getElementById(formID);
 //aqui se va a escribir el nuevo registro
-
-
+	window.requestFileSystem(window.TEMPORARY, 1024*1024, writeFile);
+	//clean form
+	form1.folio.value = "";
+	form1.monto.value = "";
+	form1.correo.value = "";
+	form1.telefono.value = "";
 }
 
 function loaded() {
 	document.getElementById("rifaForm").addEventListener("submit",
 		function(event) {
 			event.preventDefault();
-			var validEntry;
-			if(validEntry = validateEntry(this.id)) {
+			if(validateEntry(this.id)) {
 				registerEntry(this.id);
+				document.getElementById("resultadoRifa").innerHTML = "Ya fue registrado en la rifa. ¡Suerte!";
 			}else{
 				document.getElementById("resultadoRifa").innerHTML = "El ticket que usted registro no es valido";
 			}
 		},
 		false);
+}
+//filewriting
+function writeFile(fs){
+	fs.root.getFile('registroRife.txt', {create: false}, function(fileEntry) {
+		fileEntry.createWriter(function(fileWriter){
+			fileWriter.seek(fileWriter.length);
+			fileWriter.write("new Entry");
+		});
+	});
 }
 
 window.addEventListener("load", loaded, false);
